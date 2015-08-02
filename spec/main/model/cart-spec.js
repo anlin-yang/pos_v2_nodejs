78f2c7b27144228b1cd6141ae01e2aa3c53dc88f
anlin-yang <anlin0414@outlook.com>
2015-08-02 16:52:26 +0800

    use beforeEach to cart-spec.js and add test for addItem.

--- spec/main/model/cart-spec.js
+++ spec/main/model/cart-spec.js
@@ -1,36 +1,43 @@
 var Cart = require('../../../main/model/cart.js');
 
 describe("Cart", function() {
-  var theCart = new Cart();
-  var theCartItems = theCart.cartItems;
+  var theCart;
+  
+  beforeEach(function() {
+    theCart = new Cart();
+    theCart.addItem('ITEM000000', 2);
+    theCart.addItem('ITEM000001', 2);
+  });
 
   describe("constructor", function() {
-    it("should not return undifined", function() {
-      //expect(theCart.cartItems).toBeDefined();
+    it("cartItems`s type should return Array.", function() {
       expect(theCart.cartItems instanceof Array).toBe(true);
     });
   });
 
   describe("addItem", function() {
     it("should add two cartItems", function() {
-      theCart.addItem('ITEM000000', 2);
-      theCart.addItem('ITEM000001', 2);
+      expect(theCart.cartItems.length).toBe(2);
+    });
+  });
 
-      expect(theCartItems.length).toBe(2);
+  describe("addItem", function() {
+    it("cartItems length should not increase.", function() {
       theCart.addItem('ITEM000001', 2);
-
-      expect(theCartItems.length).toBe(2);
+      expect(theCart.cartItems.length).toBe(2);
     });
   });
 
   describe("getSubtotalPrice", function() {
     it("should return correct subtotalPrice", function() {
+      theCart.addItem('ITEM000001', 2);
       expect(theCart.getSubtotalPrice()).toBe(15);
     });
   });
 
   describe("getSaving", function() {
     it("should return correct saving", function() {
+      theCart.addItem('ITEM000001', 2);
       expect(theCart.getSaving()).toBe(3);
     });
   });
